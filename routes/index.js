7fa6be2e591da695f5675dcd4c8d624bc6e1ed32
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-10 18:31:54 -0700

    Install and implement luxon date formats

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { DateTime } = require("luxon");
+
+DateTime.now().toLocaleString(DateTime.DATETIME_MED)
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: DateTime.now().toLocaleString(DateTime.DATETIME_MED)
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: DateTime.now().toLocaleString(DateTime.DATETIME_MED)
   }
 ];
 
